af674db654961600b3bc9a942e62a61364b28922
ROOT\arunprabhu.sivakumar <arunprabhu.sivakumar@syncfusion.com>
2017-12-29 18:02:56 +0530

    clipper final commit

---
+++ EditWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace clipper
+{
+    /// <summary>
+    /// Interaction logic for EditWindow.xaml
+    /// </summary>
+    public partial class EditWindow : Window
+    {
+        MainWindow main = new MainWindow();
+        public EditWindow()
+        {
+            InitializeComponent();
+        }        
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            main.Visibility = Visibility.Visible;
+        }
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.SaveFileDialog dlg = new System.Windows.Forms.SaveFileDialog();
+            dlg.DefaultExt = "png";
+            dlg.Filter = "PNG|*.png|JPG|*.jpg;*.jpeg|BMP|*.bmp|GIF|*.gif";
+            DialogResult res = dlg.ShowDialog();
+            if (res == System.Windows.Forms.DialogResult.OK)
+            {
+                var encoder = new PngBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create((BitmapSource)displayImage.Source));
+                using (FileStream stream = new FileStream(dlg.FileName, FileMode.Create))
+                    encoder.Save(stream);
+            }
+        }
+
+        private void btnClipBoard_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Size size = this.RenderSize;
+            RenderTargetBitmap rtb = new RenderTargetBitmap((int)size.Width, (int)size.Height, 96, 96, PixelFormats.Pbgra32);
+
+            // Render a white background in Clipboard
+            System.Windows.Shapes.Rectangle vRect = new System.Windows.Shapes.Rectangle()
+            {
+                Width = displayImage.Width,
+                Height = displayImage.Height,
+                Fill = System.Windows.Media.Brushes.White
+            };
+            vRect.Measure(size);
+            vRect.Arrange(new Rect(size));
+            rtb.Render(displayImage);
+
+            this.Measure(size);
+            this.Arrange(new Rect(size));
+            rtb.Render(this);
+
+            System.Windows.Clipboard.SetImage(rtb);
+        }
+
+        
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace clipper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();            
+        }
+
+        private void manualClipButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            CanvasWindow canvas = new CanvasWindow();            
+            canvas.WindowState = WindowState.Maximized;
+            canvas.ShowDialog();            
+        }
+
+        private void fullClipButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();   
+            Thread.Sleep(200);
+            Bitmap screenShotBmp = new Bitmap((int)System.Windows.Forms.SystemInformation.VirtualScreen.Width , (int)System.Windows.Forms.SystemInformation.VirtualScreen.Height - 60, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            Graphics g = Graphics.FromImage(screenShotBmp);
+            g.CopyFromScreen(0, 0, 0, 0, screenShotBmp.Size, CopyPixelOperation.SourceCopy);
+            
+
+            IntPtr handle = IntPtr.Zero;
+            handle = screenShotBmp.GetHbitmap();
+            EditScreenshot editWindow = new EditScreenshot();
+            editWindow.Show();
+            var image = Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            editWindow.displayImage.Source = image;
+            Clipboard.SetImage(image);
+        }
+    }
+}
